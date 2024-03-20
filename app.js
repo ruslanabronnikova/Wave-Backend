58429f412744dba6cfb2ff29a1f73bd9b38705e2
Ruslana <bronnikovars@yandex.ru>
2024-03-20 22:15:03 +0300

    #3 Разработка контроллера для карточек

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 require('dotenv').config();
 
 const express = require('express');
+
 const app = express();
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/volnadb');
+const cardsRouter = require('./routes/cards');
+
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+
+app.use('/cards', cardsRouter);
 
 app.listen(3000, () => {
   console.log('Привет, я сервер!');
