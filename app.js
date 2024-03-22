4fba6ac076d6423a10e44fe3e6616be980af3ac4
Ruslana <bronnikovars@yandex.ru>
2024-03-22 17:26:31 +0300

    #3 Создание контроллера для пользователя

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ const app = express();
 const mongoose = require('mongoose');
 
 const cardsRouter = require('./routes/cards');
+const usersRouter = require('./routes/users');
+const { createUser, loginUser } = require('./controllers/users');
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
+app.use('/users', usersRouter);
 app.use('/cards', cardsRouter);
 
 app.listen(3000, () => {
