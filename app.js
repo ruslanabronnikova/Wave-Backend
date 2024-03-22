9304447d163c6b6913260ead79cf71ff9b991e8b
Ruslana <bronnikovars@yandex.ru>
2024-03-22 17:34:15 +0300

    #5 Разработка обработки ошибок при регистрации и входе, а также валидация форм

--- app.js
+++ app.js
@@ -7,10 +7,31 @@ const mongoose = require('mongoose');
 
 const cardsRouter = require('./routes/cards');
 const usersRouter = require('./routes/users');
+const authMiddleW = require('./middlewares/authMiddleW');
 const { createUser, loginUser } = require('./controllers/users');
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
+// Валидация запроса на вход (логин) пользователя
+app.post('/signin', celebrate({
+  body: Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+  }),
+}), loginUser);
+
+// Валидация запроса на регистрацию нового пользователя
+app.post('/signup', celebrate({
+  body: Joi.object({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().pattern(CorrectUrl),
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+  }),
+}), createUser);
+
+app.use(authMiddleW);
 app.use('/users', usersRouter);
 app.use('/cards', cardsRouter);
 
