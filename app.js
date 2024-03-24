cbbef806dcafb3fa3e1528ba11bd993e68f2f485
Ruslana <bronnikovars@yandex.ru>
2024-03-24 19:37:38 +0300

    #5 Доработка функционала входа и регистрации

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
 require('dotenv').config();
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors');
 
-const cardsRouter = require('./routes/cards');
+const cookieParser = require('cookie-parser');
+const { celebrate, Joi } = require('celebrate');
+const { errors } = require('celebrate');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const { CorrectUrl } = require('./constants/correctUrl');
 const usersRouter = require('./routes/users');
+const cardsRouter = require('./routes/cards');
+const handleNotFound = require('./routes/errorHandler');
 const authMiddleW = require('./middlewares/authMiddleW');
+const errorMiddleW = require('./middlewares/errorMiddleW');
 const { createUser, loginUser } = require('./controllers/users');
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(requestLogger);
+app.use(cors());
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 
 // Валидация запроса на вход (логин) пользователя
 app.post('/signin', celebrate({
@@ -32,8 +52,14 @@ app.post('/signup', celebrate({
 }), createUser);
 
 app.use(authMiddleW);
+
 app.use('/users', usersRouter);
 app.use('/cards', cardsRouter);
+
+app.use(errorLogger);
+app.use(handleNotFound);
+app.use(errors());
+app.use(errorMiddleW);
 
 app.listen(3000, () => {
   console.log('Привет, я сервер!');
